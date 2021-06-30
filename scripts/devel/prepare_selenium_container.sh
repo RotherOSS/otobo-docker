0f69279d81f4c4a73a1dd67690eb701a319c9c06
bernhard <Bernhard.Schmalhofer@gmx.de>
2021-06-30 19:18:44 +0200

    Issue #60: do not use hardcoded container names

--- scripts/devel/prepare_selenium_container.sh
+++ scripts/devel/prepare_selenium_container.sh
@@ -2,17 +2,17 @@
 
 # copy the needed files from the web container
 mkdir -p tmp/scripts/test
-docker cp otobo_web_1:/opt/otobo/scripts/test/sample tmp/scripts/test
+docker cp "$(docker-compose ps -q web)":/opt/otobo/scripts/test/sample tmp/scripts/test
 
 # create the target dir in the selenium container
-docker exec otobo_selenium-chrome_1 sudo mkdir /opt/otobo
-docker exec otobo_selenium-chrome_1 sudo chown seluser:seluser /opt/otobo
+docker-compose exec selenium-chrome sudo mkdir /opt/otobo
+docker-compose exec selenium-chrome sudo chown seluser:seluser /opt/otobo
 
 # copy the files
-docker cp tmp/scripts otobo_selenium-chrome_1:/opt/otobo
+docker cp tmp/scripts "$(docker-compose ps -q selenium-chrome)":/opt/otobo
 
 # make sure that the copied files are readable by seluser
-docker exec otobo_selenium-chrome_1 sudo chown -R seluser:seluser /opt/otobo
+docker-compose exec selenium-chrome sudo chown -R seluser:seluser /opt/otobo
 
 # clean up
 rm -rf tmp
